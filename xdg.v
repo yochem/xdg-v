6ac845bccbaefb8914609a9a6216ed0ebe24e5d5
Yochem van Rosmalen <git@yochem.nl>
2023-04-12 16:47:41 +0200

    First pathlib version

--- xdg.v
+++ xdg.v
@@ -9,25 +9,31 @@ module xdg
 // environment variables.
 
 import os
+import pathlib { path, Path }
 
-fn path_from_env(var string) ?string {
+fn path_from_env(var string) ?Path {
 	env_var := os.getenv(var)
 
-	// implicit also checks for empty var
-	if os.is_abs_path(env_var) {
+	if env_var == '' {
+		return none
+	}
+
+	var_path := path(env_var).resolve()
+
+	if path(var_path).is_absolute() {
 		return env_var
 	}
 
 	return none
 }
 
-fn paths_from_env(var string) ?[]string {
+fn paths_from_env(var string) ?[]Path {
 	env_var := os.getenv(var)
 
-	paths := env_var.split(':').filter(os.is_abs_path(it))
+	paths := env_var.split(':').map(path(it).resolve).filter(it.is_absolute())
 
 	if paths.len > 0 {
-		return path
+		return paths
 	}
 
 	return none
@@ -37,9 +43,9 @@ fn paths_from_env(var string) ?[]string {
 // The environment variable `$XDG_CACHE_HOME` controls the return value of this
 // function. When the variable is either not set or empty, a default equal to
 // `$HOME/.cache` is returned.
-pub fn cache_home() string {
+pub fn cache_home() Path {
 	return path_from_env('XDG_CACHE_HOME') or {
-		os.real_path(os.join_path(os.home_dir(), '.cache'))
+		path('~/.cache').resolve()
 	}
 }
 
@@ -49,9 +55,9 @@ pub fn cache_home() string {
 // The environment variable `$XDG_CACHE_HOME` controls the return value of this
 // function. When the variable is either not set or empty, a default equal to
 // `/etc/xdg` is returned.
-pub fn config_dirs() []string {
+pub fn config_dirs() []Path {
 	return path_from_env('XDG_CONFIG_DIRS') or {
-		['/etc/xdg']
+		[path('/etc/xdg')]
 	}
 }
 
@@ -59,9 +65,9 @@ pub fn config_dirs() []string {
 // The environment variable `$XDG_CONFIG_HOME` controls the return value of this
 // function. When the variable is either not set or empty, a default equal to
 // `$HOME/.config` is returned.
-pub fn config_home() string {
+pub fn config_home() Path {
 	return path_from_env('XDG_CONFIG_HOME') or {
-		os.real_path(os.join_path(os.home_dir(), '.config'))
+		path('~/.config').resolve()
 	}
 }
 
@@ -69,9 +75,9 @@ pub fn config_home() string {
 // for data files in addition to the `$XDG_DATA_HOME` base directory. If
 // $XDG_DATA_DIRS is either not set or empty, a value equal to
 // /usr/local/share/:/usr/share/ should be used.
-pub fn data_dirs() []string {
+pub fn data_dirs() []Path {
 	return path_from_env('XDG_DATA_DIRS') or {
-		['/usr/local/share', '/usr/share/']
+		[path('/usr/local/share'), path('/usr/share/')]
 	}
 }
 
@@ -79,9 +85,9 @@ pub fn data_dirs() []string {
 // The environment variable `$XDG_DATA_HOME` controls the return value of this
 // function. When the variable is either not set or empty, a default equal to
 // `$HOME/.local/share` is returned.
-pub fn data_home() string {
+pub fn data_home() Path {
 	return path_from_env('XDG_DATA_HOME') or {
-		os.real_path(os.join_path(os.home_dir(), '.local', 'share'))
+		path('~/.local/share').resolve()
 	}
 }
 
@@ -89,7 +95,7 @@ pub fn data_home() string {
 // non-essential runtime files and other file objects (such as sockets, named
 // pipes, ...) should be stored. When the variable is either not set or empty,
 // no default value is returned (the return value is optional).
-pub fn runtime_dir() ?string {
+pub fn runtime_dir() ?Path {
 	return path_from_env('XDG_RUNTIME_DIR')?
 }
 
@@ -97,8 +103,8 @@ pub fn runtime_dir() ?string {
 // The environment variable `$XDG_STATE_HOME` controls the return value of this
 // function. When the variable is either not set or empty, a default equal to
 // `$HOME/.local/state` is returned.
-pub fn state_home() string {
+pub fn state_home() Path {
 	return path_from_env('XDG_STATE_HOME') or {
-		os.real_path(os.join_path(os.home_dir(), '.local', 'state'))
+		path('~/.local/state').resolve()
 	}
 }
